b88f0e7d08f156939273de1c33e6c7ff7e617133
joshuaswan <joshuaswan@sina.com>
2017-07-24 18:35:41 +0800

    [joshua]Add LoggingTest.

---
+++ src/main/java/com/joshua/core/core/internal/Facet.java
@@ -0,0 +1,18 @@
+package com.joshua.core.core.internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by joshua on 2017/7/24.
+ */
+
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Facet {
+    Class<? extends FacetEnabler> value();
+
+    int order() default 0;
+}
---
+++ src/main/java/com/joshua/core/core/internal/FacetEnabler.java
@@ -0,0 +1,7 @@
+package com.joshua.core.core.internal;
+
+/**
+ * Created by joshua on 2017/7/24.
+ */
+public interface FacetEnabler {
+}
